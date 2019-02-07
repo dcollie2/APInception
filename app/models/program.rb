f61dfacdec48dab7e5c16f9864e7618bd3d74dca
Danny Collier <dcollie2@gmu.edu>
2019-02-07 15:29:53 -0500

    Group on degree title.

--- app/models/program.rb
+++ app/models/program.rb
@@ -1,2 +1,19 @@
 class Program < ApplicationRecord
+
+  def barebones_title
+    title.sub(", #{degree_type}",'').sub(' Minor','').sub(' Graduate Certificate','').sub(' Undergraduate Certificate','')
+  end
+
+  def interpreted_degree
+    case degree_type
+    when ''
+      then 'Minor'
+    when 'CERG'
+      then 'Graduate Certificate'
+    when 'CERB'
+      then 'Undergraduate Certificate'
+    else
+      degree_type
+    end
+  end
 end
